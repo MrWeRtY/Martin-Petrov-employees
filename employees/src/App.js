c9b22aac1b549c3aaac84f8381fb962a8586fd3a
MrWeRtY <marty5row@gmail.com>
2022-01-19 20:50:40 +0200

    Initial Source

---
+++ employees/src/App.js
@@ -0,0 +1,14 @@
+import CsvReader from './CsvReader';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CsvReader />
+      </header>
+    </div>
+  );
+}
+
+export default App;
